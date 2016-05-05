4ed4ae6255582a2d7d639f3ca2a3e5818b6178e5
cruntar <hawkingbai@gmail.com>
2016-05-04 18:48:43 -0700

    finally

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -110,4 +110,4 @@ def search():
     return render_template("map_properties_result.html", d=d, pages=pages)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=80)
